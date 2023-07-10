6c8b20d8e9f3d6647918b595d60fca8effa0d783
Your Name <quyet.tran@rikai.technology>
2023-07-10 21:46:30 +0700

    TASK_4 implement login/log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_admin_user!
+  def after_sign_in_path_for(resource)
+    '/admin'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, path: "/admin",
+    path_names: {:sign_in => "login", :sign_out => "logout"} ,
+    controllers: {
+      sessions: 'admin_users/sessions'
+    }
   get '/admin', to: 'admin/admin_users#index'
 end
